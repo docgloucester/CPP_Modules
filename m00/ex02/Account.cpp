fa89c8ac89b75ef1d2a4900479f603b7bb1e513f
rgilles <rgilles@student.42.fr>
2021-09-06 21:07:56 +0200

    prepared class cpp structure

--- m00/ex02/Account.cpp
+++ m00/ex02/Account.cpp
@@ -5,7 +5,95 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/06 19:57:35 by rgilles           #+#    #+#             */
-/*   Updated: 2021/09/06 19:58:24 by rgilles          ###   ########.fr       */
+/*   Created: 2021/09/06 20:36:18 by rgilles           #+#    #+#             */
+/*   Updated: 2021/09/06 20:55:06 by rgilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <Account.hpp>
+
+// ************************************************************************** //
+//                          Public Instance Functions                         //
+// ************************************************************************** //
+
+Account::Account(int initial_deposit) 
+{
+	
+}
+
+Account::~Account(void) 
+{
+	
+}
+
+void	Account::makeDeposit(int deposit) 
+{
+	
+}
+
+bool	Account::makeWithdrawal(int withdrawal) 
+{
+	
+}
+
+int		Account::checkAmount(void) const 
+{
+	
+}
+
+void	Account::displayStatus(void) const 
+{
+	
+}
+
+// ************************************************************************** //
+//                         Private Instance Functions                         //
+// ************************************************************************** //
+
+Account::Account(void) 
+{
+	
+}
+
+// ************************************************************************** //
+//                           Public Class Functions                           //
+// ************************************************************************** //
+
+int		Account::_nbAccounts = 0;
+int		Account::_totalAmount = 0;
+int		Account::_totalNbDeposits = 0;
+int		Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts(void) 
+{
+	return Account::_nbAccounts;
+}
+
+int Account::getTotalAmount(void) 
+{
+	return Account::_totalAmount;
+}
+
+int Account::getNbDeposits(void) 
+{
+	return Account:_totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void) 
+{
+	return Account::_totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos(void) 
+{
+	
+}
+
+// ************************************************************************** //
+//                          Private Class Functions                           //
+// ************************************************************************** //
+
+void	Account::_displayTimestamp(void) 
+{
+	
+}
