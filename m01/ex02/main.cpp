7c923d60c34a2ec9d6fd161727f4519d93af1e4b
rgilles <rgilles@student.42.fr>
2021-09-11 11:13:06 +0200

    completed spare ex02

--- m01/ex02/main.cpp
+++ m01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 00:51:47 by rgilles           #+#    #+#             */
-/*   Updated: 2021/09/09 01:00:32 by rgilles          ###   ########.fr       */
+/*   Updated: 2021/09/11 10:48:42 by rgilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,15 @@ int main(void)
 	hi = "HI THIS IS BRAIN";
 	stringPTR = &hi;
 	std::string &stringREF = hi;
+
+	std::cout << "___ADDRESSES___" << std::endl;
+	std::cout << "Address of string: " << &hi << std::endl;
+	std::cout << "Address in the pointer: " << (void*)stringPTR
+		<< ", himself being at address " << &stringPTR << std::endl;
+	std::cout << "Address of reference: " << &stringREF << std::endl;
+
+	std::cout << "____DISPLAY____" << std::endl;
+	std::cout << "directly: " << hi << std::endl;
+	std::cout << "via pointer: " << *stringPTR << std::endl;
+	std::cout << "via reference: " << stringREF << std::endl;
 }
