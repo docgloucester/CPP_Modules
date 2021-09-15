b39648d8d6133069df35b3d16c85ed5603b3af2c
rgilles <rgilles@student.42.fr>
2021-09-15 17:06:54 +0200

    progress on ex04

--- m01/ex04/main.cpp
+++ m01/ex04/main.cpp
@@ -6,26 +6,65 @@
 /*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 12:17:32 by rgilles           #+#    #+#             */
-/*   Updated: 2021/09/11 12:34:00 by rgilles          ###   ########.fr       */
+/*   Updated: 2021/09/15 15:32:46 by rgilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
+int	sanitize_input(std::fstream& input, std::fstream& output, int argc, char** argv)
+{
+	std::string		output_name;
+
+	if (argc != 4)
+	{
+		std::cout << "replace: syntax error: number of arguments isn't 4" << std::endl;
+		return (1);
+	}
+	if (*argv[2] == 0 || *argv[3] == 0)
+	{
+		std::cout << "replace: error: empty strings provided" << std::endl;
+		return (1);
+	}
+	input.open(argv[1], std::fstream::in);
+	if (!input.is_open())
+	{
+		std::cout << "replace: error opening file " << argv[1] << std::endl;
+		return (1);
+	}
+	output_name = argv[1];
+	output_name += ".replace";
+	output.open(output_name.c_str(), std::fstream::out|std::fstream::trunc);
+	if (!output.is_open())
+	{
+		std::cout << "replace: error creating file " << argv[1] << ".replace" << std::endl;
+		input.close();
+		return (1);
+	}
+	return (0);
+}
+
+
+
+
 int	main(int argc, char** argv)
 {
 	std::fstream	input;
 	std::fstream	output;
+	std::string		line;
+	std::size_t		pos;
 
-	if (argc != 4)
-		std::cout << "replace-o'matic: syntax error: number of arguments isn't 4" << std::endl;
-	if (*argv[2] == 0 || *argv[3] == 0)
-		std::cout << "replace-o'matic: error: empty strings provided" << std::endl;
-	input.open(argv[1], std::ios::in);
-	if (!input)
-		std::cout << "replace-o'matic: error opening file " << argv[1] << std::endl;
-	output.open(argv[1] << ".replace")
-	if (!output)
-		std::cout << "replace-o'matic: error creating file " << argv[1] << ".replace" << std::endl;
+	if (sanitize_input(input, output, argc, argv))
+		return (1);
+	input.getline(line, );
+	while ((pos = file.find(argv[2])) != std::string::npos)
+	{
+		file.erase(pos, std::string(argv[2]).size());
+		file.insert(pos, argv[3]);
+	}
+	output << file;
+	input.close();
+	output.close();
+	return (0);
 }
